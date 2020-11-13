4b484a32d17e38fc9ac8adaeb557df0bcaba4bd4
Vincent <vrdphone@gmail.com>
2020-11-13 14:15:18 -0600

    fixed undefined total duration

--- public/assets/js/workout.js
+++ public/assets/js/workout.js
@@ -1,12 +1,16 @@
 async function initWorkout() {
     const lastWorkout = await API.getLastWorkout();
-    console.log("Last workout:", lastWorkout);
     if (lastWorkout) {
         document.querySelector("a[href='/exercise?']").setAttribute("href", `/exercise?id=${lastWorkout._id}`);
 
+        let totalDuration = 0;
+        for (let i = 0; i < lastWorkout.exercises.length; i++) {
+            totalDuration += lastWorkout.exercises[i].duration;
+        }
+
         const workoutSummary = {
             date: formatDate(lastWorkout.day),
-            totalDuration: lastWorkout.totalDuration,
+            totalDuration: totalDuration,
             numExercises: lastWorkout.exercises.length,
             ...tallyExercises(lastWorkout.exercises)
         };
