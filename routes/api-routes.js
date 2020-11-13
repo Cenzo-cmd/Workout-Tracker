0e686e453f9d7711d2c5aa5ceec4ccc71ce4642b
Vincent <vrdphone@gmail.com>
2020-11-12 20:40:43 -0600

    added .catch to api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,24 +6,32 @@ module.exports = (app) => {
     app.get("/api/workouts", (request, response) => {
         Workout.find({}).then(data => {
             response.json(data);
+        }).catch(err => {
+            response.json(err);
         });
     });
 
     app.post("/api/workouts/", (request, response) => {
         Workout.create({}).then(data => {
             response.json(data);
+        }).catch(err => {
+            response.json(err);
         });
     });
 
     app.put("/api/workouts/:id", (request, response) => {
         Workout.findByIdAndUpdate(request.params.id, { $push: { exercises: request.body } }, { new: true }).then(data => {
             response.json(data);
+        }).catch(err => {
+            response.json(err);
         });
     });
 
     app.get("/api/workouts/range", (request, response) => {
         Workout.find({}).then(data => {
             response.json(data);
+        }).catch(err => {
+            response.json(err);
         });
     });
 };
